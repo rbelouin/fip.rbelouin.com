913b216a05d45d7f65973a4959a57548e5534fdd
Rodolphe Belouin <rodolphe.belouin@gmail.com>
2015-12-28 16:41:40 +0100

    Update controls for the song lists

--- src/js/views/player-song-list.jsx
+++ src/js/views/player-song-list.jsx
@@ -6,6 +6,7 @@ var IntlMixin = ReactIntl.IntlMixin;
 var FormattedMessage = ReactIntl.FormattedMessage;
 
 var SongListItem = React.createClass({
+  mixins: [IntlMixin],
   render: function() {
     var song = this.props.song;
 
@@ -17,14 +18,40 @@ var SongListItem = React.createClass({
       (<span className="song-list-item-year">{song.year}</span>)
     ) : "";
 
+    var favorite = (
+      <div className="song-list-item-controls-favorite" onClick={this.props.onFavorite}>
+        <span className="fa fa-heart"></span>
+        <FormattedMessage
+          message={this.getIntlMessage(song.favorite ? "remove-from-favorites" : "add-to-favorites")}
+        />
+      </div>
+    );
+
+    var spotify = song.spotify ? (
+      <a target="_blank" href={song.spotify} className="song-list-item-controls-spotify">
+        <span className="fa fa-spotify"></span>
+        <FormattedMessage
+          message={this.getIntlMessage("open-in-spotify")}
+        />
+      </a>
+    ) : "";
+
+    var controlsClassName = ["song-list-item-controls"]
+      .concat(this.props.active ? "song-list-item-controls-shown" : "")
+      .join(" ");
+
     return (
-      <li className={className}>
+      <li className={className} onClick={this.props.onClick}>
         <div className="song-list-item-title">{song.title}</div>
         <div className="song-list-item-info">
           <span className="song-list-item-artist">{song.artist}</span>
           &nbsp;-&nbsp;
           <span className="song-list-item-album">{song.album}</span>
           {year}
+        </div>
+        <div className={controlsClassName}>
+          {favorite}
+          {spotify}
         </div>
       </li>
     );
@@ -33,16 +60,32 @@ var SongListItem = React.createClass({
 
 var SongList = module.exports = React.createClass({
   mixins: [IntlMixin],
-  toggleFavorite: function(song) {
+  getInitialState: function() {
+    return {
+      selectedSongId: null
+    };
+  },
+  onFavorite: function(song) {
     this.props.favBus.push({
       type: song.favorite ? "remove" : "add",
       song: song
     });
   },
+  onSelect: function(song) {
+    var selectedSongId = song.id === this.state.selectedSongId ? null : song.id;
+
+    this.setState({selectedSongId});
+  },
   render: function() {
     var songNodes = this.props.songs.map(function(song) {
-      return <SongListItem song={song} key={song.id} />;
-    });
+      return <SongListItem
+        song={song}
+        key={song.id}
+        active={song.id === this.state.selectedSongId}
+        onClick={_.partial(this.onSelect, song).bind(this)}
+        onFavorite={_.partial(this.onFavorite, song).bind(this)}
+      />;
+    }, this);
 
     return _.isEmpty(this.props.songs) ? (
       <ul className="song-list"></ul>
