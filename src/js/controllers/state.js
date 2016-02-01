143f798de2de4db96618ac4de0f146cd05dc766f
Rodolphe Belouin <rodolphe.belouin@gmail.com>
2016-02-01 17:27:24 +0100

    Split the getState function into several smaller ones

--- src/js/controllers/state.js
+++ src/js/controllers/state.js
@@ -1,52 +1,77 @@
 import _ from "lodash";
 import Bacon from "baconjs";
 
-export function getState(SongController, favBus, token) {
-  const p_print = !token ? Bacon.constant(null) :
-                           SongController.getSpotifyPrint(token).toProperty();
+export function getPrint(SongController, token) {
+  return !token ? Bacon.constant(null) :
+                  SongController.getSpotifyPrint(token).toProperty();
+}
 
-  const p_syncs = p_print
+export function getSyncs(SongController, p_print) {
+  return p_print
     .flatMapLatest(print => SongController.getSyncs(print))
     .toProperty();
+}
 
-  const p_favSongs = p_syncs
+export function getFavSongs(SongController, p_syncs, favBus) {
+  return p_syncs
     .flatMapLatest(syncs => {
-      return SongController.getFavSongsStream(syncs, favBus).toEventStream();
+      const stream = SongController.getFavSongsStream(syncs, favBus);
+      return stream.toEventStream();
+    })
+    .toProperty();
+}
+
+export function getRadioState(SongController, p_favSongs, p_radioSongs) {
+  const p_songs = Bacon.combineWith(
+    SongController.mergeFavsAndSongs,
+    p_radioSongs,
+    p_favSongs
+  );
+
+  const p_pastSongs = p_songs.map(_.tail);
+
+  const p_nowPlaying = p_songs
+    .map(songs => _.isEmpty(songs) ? {type: "loading"} : _.head(songs))
+    .flatMapError(data => {
+      const code = data && data.error && data.error.code;
+
+      return Bacon.once(code === 100 ? {
+        type: "unknown"
+      } : new Bacon.Error(data.error));
     })
     .toProperty();
 
+  return {
+    nowPlaying: p_nowPlaying,
+    pastSongs: p_pastSongs
+  };
+}
+
+export function saveFavoriteSongs(SongController, p_syncs, p_favSongs) {
   p_syncs.flatMapLatest(syncs => {
     return p_favSongs.flatMapLatest(songs => {
       return SongController.setFavoriteSongs(syncs, songs);
     });
   }).onValue();
+}
 
-  const data = SongController.getFipSongLists();
+export function getState(SongController, favBus, token) {
+  const p_print = getPrint(SongController, token);
+  const p_user = p_print.map(print => print && print.user);
+  const p_syncs = getSyncs(SongController, p_print);
+  const p_favSongs = getFavSongs(SongController, p_syncs, favBus);
 
-  const radioSongs = _.mapValues(data, radio => {
-    const p_songs = Bacon.combineWith(
-      SongController.mergeFavsAndSongs,
-      radio,
-      p_favSongs
-    );
-
-    const p_pastSongs = p_songs.map(_.tail);
-
-    const p_nowPlaying = p_songs
-      .map(songs => _.isEmpty(songs) ? {type: "loading"} : _.head(songs))
-      .flatMapError(data => Bacon.once(data && data.error && data.error.code === 100 ? {type: "unknown"} : new Bacon.Error(data.error)))
-      .toProperty();
-
-    return {
-      nowPlaying: p_nowPlaying,
-      pastSongs: p_pastSongs
-    };
+  const songLists = SongController.getFipSongLists();
+  const radioStates = _.mapValues(songLists, p_radioSongs => {
+    return getRadioState(SongController, p_favSongs, p_radioSongs);
   });
 
+  saveFavoriteSongs(SongController, p_syncs, p_favSongs);
+
   return Bacon.combineTemplate({
-    user: p_print.map(print => print && print.user),
+    user: p_user,
     favSongs: p_favSongs,
-    radios: radioSongs
+    radios: radioStates
   });
 }
 
