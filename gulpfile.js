a274f5d044aa0b71df9fcdd5b3b896cdec740423
Rodolphe Belouin <rodolphe@spotify.com>
2017-07-06 23:13:30 +0200

    Move the configuration to a dedicated file

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,5 @@
 var gulp = require("gulp");
 var source = require("vinyl-source-stream");
-var env = require("common-env")();
-
-gulp.task("configuration", function() {
-  var defaults = require("./package.json").common_env;
-  var config = env.getOrElseAll(defaults);
-
-  var src = source("config.json");
-  src.end(JSON.stringify(config));
-
-  return src.pipe(gulp.dest("./prod/js"));
-});
 
 gulp.task("copy", function() {
   gulp.src("./src/img/icon.png")
@@ -20,4 +9,4 @@ gulp.task("copy", function() {
     .pipe(gulp.dest("./prod/public"));
 });
 
-gulp.task("build", ["configuration", "copy"]);
+gulp.task("build", ["copy"]);
