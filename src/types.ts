0db2e8774be8c805c81a19fe71f673e7ffad0b06
Rodolphe Belouin <rodolphe.belouin@gmail.com>
2019-03-18 23:15:45 +0100

    fetch radio metadata

---
+++ src/types.ts
@@ -0,0 +1,26 @@
+export type Song = {
+  id: string;
+  startTime: number;
+  endTime: number;
+  title: string;
+  album: string;
+  artist: string;
+  year: string | undefined;
+  label: string | undefined;
+  icons: { [size: string]: string };
+};
+
+export type Radio = {
+  id: string;
+  audioSource: string;
+  metadataHref: string;
+};
+
+export type NowPlaying = {
+  radio: Radio;
+  song: Song;
+};
+
+export type NowPlayingByRadio = {
+  [radioId: string]: NowPlaying;
+};
