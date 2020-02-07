9a9c3f809ea66d333519b93fd3c79b1ce823d345
Rodolphe Belouin <rodolphe.belouin@gmail.com>
2020-02-07 13:54:47 +0100

    Add support for unsafe & local HTTPS
    This is required to play with the Web MIDI API.
    More information in the [Web MIDI API specification](https://webaudio.github.io/web-midi-api/#extensions-to-the-navigator-interface).

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,7 @@
 import _ from "lodash";
+import fs from "fs";
+import http from "http";
+import https from "https";
 import qs from "querystring";
 import path from "path";
 import express from "express";
@@ -15,10 +18,10 @@ Sentry.init({
 });
 
 const app = express();
-expressWs(app);
 
 const apiPrefix = "/api";
 const publicFolder = process.env.PUBLIC_FOLDER || "";
+const certificateFolder = process.env.CERTIFICATE_FOLDER;
 const httpsOnly = process.env.HTTPS_ONLY === "1";
 const spotifyConfig = {
   clientId: process.env.SPOTIFY_CLIENT_ID || "",
@@ -29,7 +32,7 @@ const spotifyConfig = {
 
 app.use(function(req, res, next) {
   const protocol =
-    req.headers["x-forwarded-proto"] === "https" ? "https" : "http";
+    req.headers["x-forwarded-proto"] === "https" ? "https" : req.protocol;
 
   if (httpsOnly && protocol !== "https") {
     res.redirect("https://" + req.headers["host"] + req.originalUrl);
@@ -49,8 +52,21 @@ app.use(spotifyConfig.callbackPath, spotifyCallback(spotifyConfig));
 
 app.use(express.static(publicFolder));
 
-app.listen(config.port);
-console.log("Server listening on port " + config.port + "…");
+const httpServer = http.createServer(app);
+expressWs(app, httpServer);
+httpServer.listen(config.port);
+console.log("HTTP server listening on port " + config.port + "…");
+
+if (certificateFolder) {
+  const credentials = {
+    key: fs.readFileSync(path.resolve(certificateFolder, "key.pem")),
+    cert: fs.readFileSync(path.resolve(certificateFolder, "cert.pem"))
+  };
+  const httpsServer = https.createServer(credentials, app);
+  expressWs(app, httpsServer);
+  httpsServer.listen(8443);
+  console.log("HTTPS server listening on port 8443…");
+}
 
 const p_radios = fetchRadios(2000, config.radios);
 p_radios.onError(error => Sentry.captureMessage(error as any));
