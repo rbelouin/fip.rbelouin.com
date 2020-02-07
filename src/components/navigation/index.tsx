d4c10745e2682772d9002b7c726518d09a704199
Rodolphe Belouin <rodolphe.belouin@gmail.com>
2020-02-07 18:41:00 +0100

    Migrate the Navigation component

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,10 +1,98 @@
 import React from "react";
+import { FormattedMessage } from "react-intl";
 import Bacon from "baconjs";
-import PropTypes, { InferProps } from "prop-types";
+import PropTypes, { InferProps, Validator } from "prop-types";
+import { Radio } from "../../types";
 
 type BaconRoutes = typeof Bacon & {
   history: typeof window.history;
 };
+
+export const navigationPropTypes = {
+  route: PropTypes.string.isRequired,
+  radio: PropTypes.string.isRequired,
+  radios: PropTypes.arrayOf(
+    (PropTypes.object.isRequired as any) as Validator<Radio>
+  ).isRequired,
+  paneIsOpen: PropTypes.bool.isRequired
+};
+
+export type NavigationPropTypes = InferProps<typeof navigationPropTypes>;
+
+export const Navigation: React.FunctionComponent<NavigationPropTypes> = ({
+  route,
+  radio,
+  radios,
+  paneIsOpen
+}) => {
+  return (
+    <nav className={`app-nav ${paneIsOpen ? "app-nav-open" : "app-nav-close"}`}>
+      <ul>
+        <li
+          className={`app-nav-group ${isRadiosActive(route) ? "active" : ""}`}
+        >
+          <div>
+            <FormattedMessage id="radios" />
+          </div>
+          <ul>
+            {radios.map(radioItem => (
+              <NavigationItem
+                key={radioItem.id}
+                href={getRadioUrl(radioItem)}
+                messageId={radioItem.id}
+                active={isRadiosActive(route) && radio === radioItem.id}
+              />
+            ))}
+          </ul>
+        </li>
+        <NavigationItem
+          href="/users/me/songs"
+          messageId="favorites"
+          active={isFavoritesActive(route)}
+        />
+      </ul>
+    </nav>
+  );
+};
+
+Navigation.propTypes = navigationPropTypes;
+export default Navigation;
+
+function getRadioUrl(radio: Radio): string {
+  return `/radios/${radio.id}`;
+}
+
+function isFavoritesActive(route: string) {
+  return route === "favorites";
+}
+
+function isRadiosActive(route: string) {
+  return route === "radio";
+}
+
+export const navigationItemPropTypes = {
+  href: PropTypes.string.isRequired,
+  messageId: PropTypes.string.isRequired,
+  active: PropTypes.bool
+};
+
+export type NavigationItemPropTypes = InferProps<
+  typeof navigationItemPropTypes
+>;
+
+export const NavigationItem: React.FunctionComponent<NavigationItemPropTypes> = ({
+  href,
+  messageId,
+  active
+}) => (
+  <li className={active ? "active" : ""}>
+    <Link href={href}>
+      <FormattedMessage id={messageId} />
+    </Link>
+  </li>
+);
+
+NavigationItem.propTypes = navigationItemPropTypes;
 
 export const linkPropTypes = {
   href: PropTypes.string.isRequired,
@@ -21,6 +109,8 @@ export const Link: React.FunctionComponent<LinkPropTypes> = ({
     {children}
   </a>
 );
+
+Link.propTypes = linkPropTypes;
 
 const onLinkClick = (href: string) => (
   event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
